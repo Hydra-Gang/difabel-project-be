755cfe6289f6830a0203d2390315eaac7a787116
Alviannn <alvianndq@gmail.com>
2022-03-19 23:12:50 +0700

    Add `User` model and (incomplete) auth controller

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,33 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+/**
+ * Describes the user's current role
+ */
+export enum AccessLevels {
+    ADMIN,
+    EDITOR,
+    CONTRIBUTOR
+}
+
+@Entity('users')
+export class User extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ name: 'full_name', length: 64 })
+    fullName!: string;
+
+    @Column({ length: 64 })
+    email!: string;
+
+    @Column({ length: 32 })
+    phone!: string;
+
+    @Column({ length: 64 })
+    password!: string;
+
+    @Column({ name: 'access_level', type: 'bit' })
+    accessLevel!: AccessLevels;
+
+}
