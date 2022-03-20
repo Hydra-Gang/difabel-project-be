708d48f1e153f6301ced0d9bbcc99527e3057ea3
Alviannn <alvianndq@gmail.com>
2022-03-20 22:52:58 +0700

    Add article validation

---
+++ src/validations/article.validation.ts
@@ -0,0 +1,16 @@
+import joi from 'joi';
+
+export type NewArticleType = {
+    title: string,
+    content: string
+}
+
+export const newArticleSchema = joi.object({
+    title: joi.string()
+        .max(128)
+        .required(),
+
+    content: joi.string()
+        .max(2_000)
+        .required()
+});
