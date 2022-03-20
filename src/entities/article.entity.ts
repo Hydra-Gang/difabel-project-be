11ba95ec5de01a98d9c43299c8404f435d9fc56a
Alviannn <alvianndq@gmail.com>
2022-03-20 22:52:48 +0700

    Add article entity and it's relations

---
+++ src/entities/article.entity.ts
@@ -0,0 +1,40 @@
+import { User } from './user.entity';
+import {
+    BaseEntity, Entity,
+    Column, PrimaryGeneratedColumn, JoinColumn,
+    ManyToOne
+} from 'typeorm';
+
+@Entity('articles')
+export class Article extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ length: 128 })
+    title!: string;
+
+    @Column({ length: 2_000 })
+    content!: string;
+
+    @Column({ name: 'created_at', type: 'date', default: new Date() })
+    createdAt!: Date;
+
+    @Column({ name: 'updated_at', type: 'date', default: new Date() })
+    updatedAt!: Date;
+
+    @Column({ name: 'is_deleted' })
+    isDeleted!: boolean;
+
+    @Column({ name: 'is_approved' })
+    isApproved!: boolean;
+
+    @ManyToOne(() => User, (user) => user.articles)
+    @JoinColumn({ name: 'author_id' })
+    author!: User;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'approver_id' })
+    approver!: User;
+
+}
