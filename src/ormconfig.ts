545bb4469a1fd50a9c0563bc2ed1aa8bb53df06c
Alviannn <alvianndq@gmail.com>
2022-04-05 16:44:23 +0700

    * Add support for development environment * Fix typeorm CLI

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,6 +3,32 @@ import config from './configs/config';
 
 import { DataSource } from 'typeorm';
 
+type ORMPathType = 'entities' | 'migrations' | 'subscribers';
+const isDev = process.env.NODE_ENV === 'development';
+
+/**
+ * Provides the path to the specific ORM directories
+ */
+function pathToLoadORM(type: ORMPathType) {
+    const startDir = (isDev ? 'src' : 'dist');
+    const lastExtension = (isDev ? 'ts' : 'js');
+
+    let middleExtension: string;
+    switch (type) {
+        case 'entities':
+            middleExtension = 'entity';
+            break;
+        case 'migrations':
+            middleExtension = 'migration';
+            break;
+        case 'subscribers':
+            middleExtension = 'subscriber';
+            break;
+    }
+
+    return `${startDir}/${type}/**/*.${middleExtension}.${lastExtension}`;
+}
+
 export const appDataSource = new DataSource({
     type: 'postgres',
     host: config.db.host,
@@ -10,8 +36,8 @@ export const appDataSource = new DataSource({
     username: config.db.username,
     password: config.db.password,
     database: config.db.database,
-    synchronize: true,
-    entities: ['dist/entities/**/*.js'],
-    migrations: ['dist/migrations/**/*.js'],
-    subscribers: ['dist/subscribers/**/*.js']
+    synchronize: isDev,
+    entities: [pathToLoadORM('entities')],
+    migrations: [pathToLoadORM('migrations')],
+    subscribers: [pathToLoadORM('subscribers')]
 });
